5eb0abaf7c59ce2278f704b9f8af360949952baf
CJ Wood <cjwood0@gmail.com>
2019-10-04 19:35:20 -0400

    post model clean up

--- server/models/post.js
+++ server/models/post.js
@@ -1,9 +1,8 @@
-const mongoose = require('mongoose');
-
-const postSchema = mongoose.Schema({
-  hy: { type: String, required: true, minlength: 1, maxlength: 200 },
-  creator: {type: mongoose.SchemaTypes.ObjectId, ref: "User", required: true },
-  likes: 0
-});
+const mongoose = require('mongoose'),
+      postSchema = mongoose.Schema({
+        hy: { type: String, required: true, minlength: 1, maxlength: 200 },
+        creator: { type: idType, ref: "User", required: true },
+        likes: 0
+      });
 
 module.exports = mongoose.model('Post', postSchema);

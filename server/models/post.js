00f3cc6b5a8f6712b2ce7e09f835dfd697861b0a
CJ Wood <cjwood0@gmail.com>
2019-10-04 19:57:54 -0400

    post.content
    don't get cute in the code

--- server/models/post.js
+++ server/models/post.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose'),
       postSchema = mongoose.Schema({
-        hy: { type: String, required: true, minlength: 1, maxlength: 200 },
+        content: { type: String, required: true, minlength: 1, maxlength: 200 },
         creator: { type: idType, ref: "User", required: true },
         likes: 0
       });

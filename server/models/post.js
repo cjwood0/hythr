19ecb3c2f25d5e5992359792b0a104b90edfaedc
CJ Wood <cjwood0@gmail.com>
2019-10-04 19:22:59 -0400

    posts start with no likes

--- server/models/post.js
+++ server/models/post.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const postSchema = mongoose.Schema({
   hy: { type: String, required: true },
   creator: {type: mongoose.SchemaTypes.ObjectId, ref: "User", required: true },
-  likes: { type: Number }
+  likes: { type: 0 }
 });
 
 module.exports = mongoose.model('Post', postSchema);

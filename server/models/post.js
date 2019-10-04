315376ecba4734df84f55783d0cd480a3c8b5b35
CJ Wood <cjwood0@gmail.com>
2019-10-04 19:23:55 -0400

    Post length

--- server/models/post.js
+++ server/models/post.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const postSchema = mongoose.Schema({
-  hy: { type: String, required: true },
+  hy: { type: String, required: true, minlength: 1, maxlength: 200 },
   creator: {type: mongoose.SchemaTypes.ObjectId, ref: "User", required: true },
   likes: { type: 0 }
 });

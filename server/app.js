d9983e0c330a3098c7a67d175b6253ba619853a1
CJ Wood <cjwood0@gmail.com>
2019-10-04 23:58:06 -0400

    connect to the db
    this took me half the time it took to make a button look ok

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      mongoose = require('mongoose'),
+      app = express();
+
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect('mongodb+srv://hythr:' + process.env.MONGO_PASSWORD + '@cluster0-lnezh.mongodb.net/hythr?w=majority', { useNewUrlParser: true }).then(() => {
+  console.log("Connected to DB");
+}).catch(() => {
+  console.log("connection failed!"); // wat do
+}); // all the sharing feels so weird
 
 app.use(bodyParser.json());
-
 
 module.exports = app;

e5dd82ccb19c9aba3c851ebe805abdcd2b6b777f
CJ Wood <cjwood0@gmail.com>
2019-10-04 23:53:18 -0400

    Wire up header routing
    more time spent styling than anything

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PostListComponent } from './components/posts/post-list/post-list.component';
+import { PostCreateComponent } from './components/posts/post-create/post-create.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
 
 
 const routes: Routes = [
-  { path: '', component: PostListComponent }
+  { path: '', component: PostListComponent },
+  { path: 'create', component: PostCreateComponent },
+  { path: 'auth/login', component: LoginComponent },
+  { path: 'auth/signup', component: SignupComponent }
 ];
 
 @NgModule({

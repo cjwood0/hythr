b0c60e4d4ee0caa0b6584226c12ab5b2990c5d5a
CJ Wood <cjwood0@gmail.com>
2019-10-04 21:03:55 -0400

    control the posts

---
+++ server/controllers/posts.js
@@ -0,0 +1,51 @@
+const Post = require('../models/post');
+
+exports.createPost = (req, res, next) => {
+  const post = new Post({
+          content: req.body.content,
+          creator: req.user.id
+        });
+
+  post.save().then(newPost => {
+    res.status(201).json({
+      id: newPost._id,
+      content: newPost.content
+    });
+  }).catch(error => {
+    res.status(500).json({ message: 'Creating post failed' });
+  });
+};
+
+exports.getPost = (req, res, next) => {
+  Post.findById(req.params.id).then(post => {
+    if (post) { // I hate lint
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: 'Post not found' });
+    }
+  });
+};
+
+exports.getPosts = (req, res, next) => {
+  const pageSize = +req.query.pagesize, // neat trick
+        currentPage = +req.query.page,
+        postQuery = Post.find(); // TODO: figure out if is this lazy
+
+  let posts;
+
+  if (pageSize && currentPage) {
+    postQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
+  }
+
+  postQuery.then(documents => { // I wish they were records...
+    posts = documents;
+    return Post.countDocuments();
+  }).then(postCount => {
+    res.status(200).json({
+      posts,
+      postCount
+    });
+  }).catch(error => {
+    return res.status(500).json({ message: 'Error getting posts' });
+  });
+};

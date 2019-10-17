e5eaee9a123bed5f4651fba84072d962ed6f2192
CJ Wood <cjwood0@gmail.com>
2019-10-16 22:37:35 -0400

    [WIP] Server side auth middleware

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,9 @@
 const express = require('express'),
       router = express.Router(),
+      checkAuth = require('../middleware/check-auth'),
       PostsController = require('../controllers/posts');
 
-router.post("", PostsController.createPost);
+router.post("", checkAuth, PostsController.createPost);
 router.get('/:id', PostsController.getPost);
 router.get('', PostsController.getPosts);
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,11 @@
 const express = require('express'),
       router = express.Router(),
+      checkAuth = require('../middleware/check-auth'),
       UsersController = require('../controllers/users');
 
 router.post('/signup', UsersController.createUser);
 router.post('/login', UsersController.loginUser);
-router.put('/follow', UsersController.follow);
-router.put('/unfollow', UsersController.unfollow);
+router.put('/follow', checkAuth, UsersController.follow);
+router.put('/unfollow', checkAuth, UsersController.unfollow);
 
 module.exports = router;

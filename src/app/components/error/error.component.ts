000534dec10c8696b532be653504ac939057072b
CJ Wood <cjwood0@gmail.com>
2019-10-26 22:13:47 -0400

    Make error component now, because there will be errors...

--- src/app/components/error/error.component.ts
+++ src/app/components/error/error.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
   styleUrls: ['./error.component.sass']
 })
-export class ErrorComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class ErrorComponent {
+  message = 'An unknown error occurred';
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {message: string}) { }
 
 }

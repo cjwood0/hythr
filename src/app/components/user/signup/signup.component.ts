7fadf7cc3652c5ef36cfa1569c69b67942b5dce9
CJ Wood <cjwood0@gmail.com>
2019-10-26 23:55:16 -0400

    Signup actually works!

--- src/app/components/user/signup/signup.component.ts
+++ src/app/components/user/signup/signup.component.ts
@@ -1,23 +1,30 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UserService } from '../user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.sass']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit, OnDestroy {
+  private userStatusSubscription: Subscription;
+
   constructor(public userService: UserService) { }
 
   ngOnInit() {
+    this.userStatusSubscription = this.userService.getUserStatusListener().subscribe(authStatus => { });
   }
 
   onSignup(form: NgForm) {
     if (form.invalid) {
       return;
     }
-    console.log(form);
     this.userService.createUser(form.value.name, form.value.email, form.value.password);
+  }
+
+  ngOnDestroy() {
+    this.userStatusSubscription.unsubscribe();
   }
 }

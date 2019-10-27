1a2a10d4334be8bb752fc9af972ac075e030d321
CJ Wood <cjwood0@gmail.com>
2019-10-26 23:15:32 -0400

    authentication baby steps in the header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,20 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UserService } from '../user/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  userIsAuthenticated = false;
+  private userListenerSubscription: Subscription;
 
   constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.userIsAuthenticated = this.userService.getIsAuthenticated();
+    this.userListenerSubscription = this.userService.getUserStatusListener().subscribe(isAuthenticated => {
+      this.userIsAuthenticated = isAuthenticated;
+    });
   }
 
   onLogout() {
     this.userService.logout();
   }
 
+  ngOnDestroy() {
+    this.userListenerSubscription.unsubscribe();
+  }
 }

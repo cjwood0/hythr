635377db927fcf76acb7d72ddcc37efcb47664e8
CJ Wood <cjwood0@gmail.com>
2019-10-28 03:07:29 -0400

    use the userguard for post creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,19 @@ import { PostListComponent } from './components/posts/post-list/post-list.compon
 import { PostCreateComponent } from './components/posts/post-create/post-create.component';
 import { LoginComponent } from './components/user/login/login.component';
 import { SignupComponent } from './components/user/signup/signup.component';
+import { UserGuard } from './components/user/user.guard';
 
 
 const routes: Routes = [
   { path: '', component: PostListComponent },
-  { path: 'create', component: PostCreateComponent },
+  { path: 'create', component: PostCreateComponent, canActivate: [UserGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [UserGuard]
 })
 export class AppRoutingModule { }

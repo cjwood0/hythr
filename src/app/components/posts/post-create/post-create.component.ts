55dcf8845e67acba17bf2334557dcf28bb97dc47
CJ Wood <cjwood0@gmail.com>
2019-10-28 06:31:38 -0400

    auth for post create

--- src/app/components/posts/post-create/post-create.component.ts
+++ src/app/components/posts/post-create/post-create.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PostsService } from '../posts.service';
 import { Post } from '../post.model';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../user/user.service';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.sass']
 })
-export class PostCreateComponent implements OnInit {
+export class PostCreateComponent implements OnInit, OnDestroy {
 
-  constructor(public postsService: PostsService) { }
+  private userStatusSubscription: Subscription;
+
+  userIsAuthenticated = false;
+
+  constructor(public postsService: PostsService, public route: ActivatedRoute, private userService: UserService) { }
 
   ngOnInit() {
+    this.userStatusSubscription = this.userService.getUserStatusListener().subscribe(isAuthenticated => {
+      this.userIsAuthenticated = isAuthenticated;
+    });
   }
 
   onCreatePost(form: NgForm) {
@@ -22,5 +32,9 @@ export class PostCreateComponent implements OnInit {
 
     this.postsService.createPost(form.value.content);
     form.reset();
+  }
+
+  ngOnDestroy() {
+    this.userStatusSubscription.unsubscribe();
   }
 }

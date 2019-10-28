bb0ba45fcedff82c2695c3107f51efe248c0e763
CJ Wood <cjwood0@gmail.com>
2019-10-28 06:32:45 -0400

    maintain auth status check in post list
    need to modularize this

--- src/app/components/posts/post-list/post-list.component.ts
+++ src/app/components/posts/post-list/post-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
 import { Subscription  } from 'rxjs';
+import { UserService } from '../../user/user.service';
 
 @Component({
   selector: 'app-post-list',
@@ -9,10 +10,12 @@ import { Subscription  } from 'rxjs';
   styleUrls: ['./post-list.component.sass']
 })
 export class PostListComponent implements OnInit, OnDestroy {
-  constructor(public postsService: PostsService) { }
+  constructor(public postsService: PostsService, private userService: UserService) { }
 
   private postsSub: Subscription;
-
+  private userStatusSubscription: Subscription;
+  userIsAuthenticated = false;
+  userId: string;
   posts: Post[] = [];
   totalPosts = 0;
 
@@ -22,10 +25,17 @@ export class PostListComponent implements OnInit, OnDestroy {
       this.totalPosts = postData.postCount;
       this.posts = postData.posts;
     });
+
+    this.userIsAuthenticated = this.userService.getIsAuthenticated();
+    this.userStatusSubscription = this.userService.getUserStatusListener().subscribe(isAuthenticated => {
+      this.userIsAuthenticated = isAuthenticated;
+      this.userId = this.userService.getUserId();
+    });
   }
 
   ngOnDestroy() {
     this.postsSub.unsubscribe();
+    this.userStatusSubscription.unsubscribe();
   }
 
   follow(followId: string) {

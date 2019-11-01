ed7458c27e871de7d17f3cc2ea5b28d0af13459f
CJ Wood <cjwood0@gmail.com>
2019-11-01 01:45:54 -0400

    I got one test to work!

--- src/app/components/error/error.component.spec.ts
+++ src/app/components/error/error.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ErrorComponent } from './error.component';
+import { MAT_DIALOG_DATA } from '@angular/material';
 
 describe('ErrorComponent', () => {
   let component: ErrorComponent;
@@ -8,7 +9,11 @@ describe('ErrorComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ErrorComponent ]
+      declarations: [ ErrorComponent ],
+      providers: [{
+        provide: MAT_DIALOG_DATA,
+        useValue: {} // Add any data you wish to test if it is passed/used correctly
+      }]
     })
     .compileComponents();
   }));
